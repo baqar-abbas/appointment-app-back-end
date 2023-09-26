b1c95fbdbb972fedfbb2bdd0ee87277626d97b1f
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-09-27 02:37:48 +0600

    Define Associations for the Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :doctor_appointments, foreign_key: 'doctor_id', class_name: 'Appointment'
+  has_many :patient_appointments, foreign_key: 'patient_id', class_name: 'Appointment'
 end
