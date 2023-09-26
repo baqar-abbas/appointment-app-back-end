130885b1bd364222ef592285fbd4cc01b031c435
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-09-26 16:42:17 -0400

    Defined validations for the models

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,5 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor, class_name: 'User'
   belongs_to :patient, class_name: 'User'
+  validates :appointment_date, :doctor_id, :patient_id, :status, :location, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :doctor_appointments, foreign_key: 'doctor_id', class_name: 'Appointment'
   has_many :patient_appointments, foreign_key: 'patient_id', class_name: 'Appointment'
+  validates :name, :email, :photo, :age, :role, :address, presence: true
+  validates :password, :password_confirmation, presence: true, length: { minimum: 6 }
 end
