97a8459862f8f1fcf327e12dd1eeb7c860cc228e
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-09-27 15:02:18 +0600

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :doctor_appointments, foreign_key: 'doctor_id', class_name: 'Appointment'
   has_many :patient_appointments, foreign_key: 'patient_id', class_name: 'Appointment'
   validates :name, :email, :photo, :age, :role, :address, presence: true
